6889f66c8c26ea4f10233fd14782d03ca8790065
SIDDAM-MOUNIKA <ssmonika894@gmail.com>
2024-10-29 19:06:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function showAlert() {
+    alert("Andhra Pradesh offers a diverse range of destinations, including temples, beaches, and hill stations. Explore more to plan your next adventure!");
+}
